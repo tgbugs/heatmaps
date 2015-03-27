d8b234ef2a7df143fc97c138b579808acb3592e1
Tom Gillespie <tgbugs@gmail.com>
2015-03-26 23:21:13 -0700

    added the sql needed to set up the user, database, and schema we need for this to run

---
+++ heatmap_db_setup.sql
@@ -0,0 +1,60 @@
+--create the user
+CREATE ROLE heatmapuser LOGIN
+    NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE;
+
+--create the db
+CREATE DATABASE heatmap_test
+    WITH OWNER = heatmapuser
+    ENCODING = 'UTF8'
+    TABLESPACE = pg_default
+    LC_COLLATE = 'en_US.UTF-8'
+    LC_CTYPE = 'en_US.UTF-8'
+    CONNECTION LIMIT = -1;
+
+GRANT ALL ON DATABASE heatmap_test TO heatmapuser;
+
+CREATE SCHEMA heatmap
+    AUTHORIZATION heatmapuser;
+
+GRANT ALL ON SCHEMA heatmap TO heatmapuser;
+
+--now connect to the database and make some tables
+CREATE EXTENSION hstore SCHEMA heatmap;
+
+SET SCHEMA 'heatmap';
+
+CREATE TABLE heatmap_prov(
+    id integer,
+    doi text,
+    "DateTime" timestamp without time zone,
+    CONSTRAINT heatmap_prov_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE term_history(
+    heatmap_prov_id integer,
+    term text,
+    term_hstore hstore,
+    CONSTRAINT heatmap_prov_id_fkey FOREIGN KEY (heatmap_prov_id)
+        REFERENCES heatmap_prov (id) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+CREATE TABLE term_hstores(
+    term text,
+    src_counts hstore
+);
+
+/*  I almost think that we don't need this since we are just going to hold all
+the data in memory anyway and can repopulate from  * if needed, mappings from
+view_nif_id -> names and other things should not be handled here, they are
+already well managed somewhere else
+
+CREATE TABLE summary_view_entity(
+    view_nif_id text,
+    name text,
+);
+*/
+
+ALTER TABLE heatmap_prov OWNER TO heatmapuser;
+ALTER TABLE term_history OWNER TO heatmapuser;
+ALTER TABLE term_hstores OWNER TO heatmapuser;
