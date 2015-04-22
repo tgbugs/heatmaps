f2cb7201ab89c779c2cc4245b41b7082f7d26c7d
Tom Gillespie <tgbugs@gmail.com>
2015-04-22 00:45:45 -0700

    added hmcli to act as a docopt interface to db creation and setup

---
+++ hmcli.py
@@ -0,0 +1,33 @@
+"""
+Usage:
+    hmcli.py [ --createdb --setupdb --test ]
+    heatmaps.py -h | --help
+Options:
+    -c --createdb   create and set up the db (implies --setupdb)
+    -s --setupdb    set up the database if it has already been created
+    -t --test       run some simple tests (wont work for production)
+"""
+from docopt import docopt
+from IPython import embed
+
+from dbsetup import setup_db
+from dbtest import test
+from services import term_service, ontology_service, heatmap_service
+
+def main():
+
+
+    args = docopt(__doc__, version='heatmaps .0001')
+    if args['--createdb']:
+        setup_db(True)
+    elif args['--setupdb']:
+        setup_db()
+
+    if args['--test']:
+        test(heatmap_service)
+
+
+
+
+
+
