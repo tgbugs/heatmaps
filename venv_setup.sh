1fbc74cdb19680648f08d65af77047489980f3e9
Tom Gillespie <tgbugs@gmail.com>
2015-07-01 18:52:31 -0700

    bug squashing for venv

--- venv_setup.sh
+++ venv_setup.sh
@@ -1,4 +1,4 @@
-# create our venv for deploy
+# create our venv for deploy, run as root
 PYVENV="/usr/local/anaconda3/bin/pyvenv"
 BUILD_DIR="/tmp/venv_build"
 INSTALL_DIR="/var/virtualenvs/"
@@ -12,7 +12,8 @@ pip install setuptools psycopg2 requests lxml numpy flask ipython mod_wsgi
 pip freeze > heatmap_venv/requirements.txt
 mod_wsgi-express install-module
 deactivate
-mv headmap_venv $INSTALL_DIR
+mkdir $INSTALL_DIR
+mv heatmap_venv $INSTALL_DIR
 cd ..
 rmdir $BUILD_DIR
 
