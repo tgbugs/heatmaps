aa358730f51837120b1415f509d50796cafb674f
Tom Gillespie <tgbugs@gmail.com>
2015-07-01 17:25:38 -0700

    clean up old build dirs

--- apch_setup.sh
+++ apch_setup.sh
@@ -2,6 +2,7 @@
 BUILD_DIR="/tmp/heatmap_build"
 VENV_ZIP="/tmp/venv_build/venv_deploy.zip"
 
+rm -rf $BUILD_DIR
 mkdir $BUILD_DIR
 cd $BUILD_DIR
 git clone https://github.com/tgbugs/heatmaps.git
--- venv_setup.sh
+++ venv_setup.sh
@@ -2,6 +2,8 @@
 PYVENV="/usr/local/anaconda3/bin/pyvenv"
 BUILD_DIR="/tmp/venv_build"
 
+rm -rf $BUILD_DIR
+mkdir $BUILD_DIR
 cd $BUILD_DIR
 $PYVENV venv
 source venv/bin/activate
