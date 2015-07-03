367f13915f3b40848311304872e65e4c0f85deb5
Tom Gillespie <tgbugs@gmail.com>
2015-07-02 19:48:26 -0700

    venv setup needs to be run on the production server, changes made accordingly

--- venv_setup.sh
+++ venv_setup.sh
@@ -1,20 +1,15 @@
 # create our venv for deploy, run as root
 PYVENV="/usr/local/anaconda3/bin/pyvenv"
-BUILD_DIR="/tmp/venv_build"
 INSTALL_DIR="/var/virtualenvs/"
+VENV=heatmap_venv
 
-rm -rf $BUILD_DIR
-mkdir $BUILD_DIR
-cd $BUILD_DIR
-$PYVENV heatmap_venv
-source heatmap_venv/bin/activate
-pip install setuptools psycopg2 requests lxml numpy flask ipython mod_wsgi
-pip freeze > heatmap_venv/requirements.txt
-mod_wsgi-express install-module
+mkdir -p $INSTALL_DIR
+rm -rf $INSTALL_DIR$VENV &&
+cd $INSTALL_DIR &&
+$PYVENV $VENV &&
+source heatmap_venv/bin/activate &&
+pip install setuptools psycopg2 requests lxml numpy flask ipython mod_wsgi &&
+pip freeze > heatmap_venv/requirements.txt &&
+mod_wsgi-express install-module &&
 deactivate
-mkdir $INSTALL_DIR
-rm -rf $INSTALL_DIR/heatmap_venv
-mv heatmap_venv $INSTALL_DIR
-cd ..
-rmdir $BUILD_DIR
 
