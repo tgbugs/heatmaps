0d8f09d35036874874cd8759d0982c8a8dca023e
Benjamin Li <benlibenli@gmail.com>
2016-05-26 13:48:19 -0700

    Comment out conditional statement around lines 1326-1334

--- heatmaps/services.py
+++ heatmaps/services.py
@@ -1322,6 +1322,8 @@ class heatmap_service(database_service):
 
         if term_coll_function:
             term_id_coll_dict, term_id_name_dict = term_coll_function(heatmap_data, term_id_name_dict)
+            
+"""
             if idSortSources not in term_id_coll_dict:  # note that idSortSources should be a TERM identifier
                 idSortSources = idSortSources.rsplit('-',1)[0]
                 if idSortSources not in term_id_coll_dict:
@@ -1329,6 +1331,7 @@ class heatmap_service(database_service):
                     raise NameError('Identifier %s unknown!' % idSortSources)
         else:
             term_id_coll_dict = None
+"""
 
         # sources
         if collSources == 'cheese':
